6f1b32625eb895f2dfced8a5778b4c8da4c9cdd2
furlupe <101555696+furlupe@users.noreply.github.com>
2022-11-28 19:05:58 +0700

    AuthException throwing added

--- services/BasketService.php
+++ services/BasketService.php
@@ -1,9 +1,13 @@
 <?php
     include_once dirname(__DIR__, 1)."/helpers/Token.php";
     include_once dirname(__DIR__, 1)."/models/DishBasketDTO.php";
+    include_once dirname(__DIR__, 1)."/exceptions/AuthException.php";
     class BasketService {
         public static function getBasket() {
             $userId = Token::getIdFromToken($GLOBALS["USER_TOKEN"]);
+            if(is_null($userId)) {
+                throw new AuthException();
+            }
             $basket = $GLOBALS["LINK"]->query(
                 "SELECT dishId, amount
                 FROM BASKET
